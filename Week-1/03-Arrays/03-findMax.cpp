1b38f1287d1c254c3674cadae77212e90f0a27be
Mohd Adnan <mohdadnan3508@gmail.com>
2025-07-08 17:24:14 +0530

    Find max element in a 2d array

---
+++ Week-1/03-Arrays/03-findMax.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+// Find Max element in a 2D array
+int findMax(int arr[][4], int row, int col) {
+
+    int maxAns = INT_MIN;
+
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<col; j++) {
+            // Check if the current element is greater than maxAns
+            // If it is, update maxAns
+            if(arr[i][j] > maxAns) {
+                maxAns = arr[i][j]; 
+            }
+        }
+    }
+
+    return maxAns;
+
+}
+
+int main() {
+
+    int arr[3][4] = {
+        {1, 22, 3, 4},
+        {5, 60, 77, 8},
+        {9, 10, 11, 12}
+    };
+
+    int row = 3, col = 4;
+    int target = 11;
+
+    cout << "Printing the max No. : " << findMax(arr, row, col) << endl;
+
+} 
