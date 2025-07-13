2c836863a3c1b37e7cd777571dea28550cd7b455
Mohd Adnan <mohdadnan3508@gmail.com>
2025-07-13 17:32:30 +0530

    Selection Sort For Desending order

---
+++ Week-3/02-Selection-Sort-Des.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int arr[6] = {10, 4, 1, 3, 2, 7};
+    int n = 6;
+
+    for(int i=0; i<n-1; i++) {
+        int index = i;
+        
+        for(int j=i+1; j<n; j++) {
+            if(arr[j] > arr[index]){
+                index = j;
+            }
+        }
+
+       swap(arr[i], arr[index]);
+    }
+
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " "; 
+    }
+}
