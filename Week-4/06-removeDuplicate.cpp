a1ed971f57f3ecb0012e3c1bb4536095d4d24c84
Mohd Adnan <mohdadnan3508@gmail.com>
2025-07-16 11:15:12 +0530

    Remove Duplicate Adjacent String

--- Week-4/06-removeDuplicate.cpp
+++ Week-4/06-removeDuplicate.cpp
@@ -2,33 +2,30 @@
 #include <string>
 using namespace std;
 
-class Solution {
-public:
-    string removeDuplicates(string s) {
-        string ans = "";
-        int index = 0;
+// Function to remove adjacent duplicates
+string removeDuplicates(string s) {
+    string ans = "";
+    int index = 0;
 
-        while (index < s.length()) {
-            if (!ans.empty() && ans.back() == s[index]) {
-                ans.pop_back(); // Remove last character
-            } else {
-                ans.push_back(s[index]); // Add current character
-            }
-            index++;
+    while (index < s.length()) {
+        if (!ans.empty() && ans.back() == s[index]) {
+            ans.pop_back();  // remove last character if same
+        } else {
+            ans.push_back(s[index]);  // add character
         }
-
-        return ans;
+        index++;
     }
-};
+
+    return ans;
+}
 
 int main() {
     string input;
     cout << "Enter a string: ";
     cin >> input;
 
-    Solution obj;
-    string result = obj.removeDuplicates(input);
-    
+    string result = removeDuplicates(input);
+
     cout << "After removing duplicates: " << result << endl;
 
     return 0;
