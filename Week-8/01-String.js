0a2e46448f02e26fc32f830515e2eb28f5c53960
Mohd Adnan <mohdadnan3508@gmail.com>
2025-08-03 11:29:59 +0530

    Basic String Method

---
+++ Week-8/01-String.js
@@ -0,0 +1,28 @@
+// Craetion String
+
+let str = "Adnan";
+
+// Print
+
+console.log(str);
+
+// index print
+console.log(str[1]) // => d
+
+// Ascii Value
+console.log(str[0].charCodeAt());
+
+// uppercase and lowercase
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+// Extracts a part of a string.
+
+str.substring(1, 2)
+
+// Loop through a String
+
+for(let i=0; i<str.length; i++) {
+    console.log(str[i])
+}
